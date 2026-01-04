5b78a659c029cf8689b5f92a26c0fe7ee6e4a43e
Glitch <pulse7151@gmail.com>
2026-01-04 20:17:26 +0400

    Create bundle.js

---
+++ bundle.js
@@ -0,0 +1,7 @@
+const url = new URL('https://google.com');
+let to = new URLSearchParams(location.search).get('to');
+if (to) {
+  if (!to.startsWith('/')) { to = '/' + to; }
+} else { to = ''; }
+const redirect = url + to;
+location.replace(redirect);
