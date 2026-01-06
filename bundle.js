be7bdcb2142b91cb13464408c8caff3573ed5eb9
Glitch <pulse7151@gmail.com>
2026-01-06 10:57:38 +0400

    Update bundle.js

--- bundle.js
+++ bundle.js
@@ -1,5 +1,5 @@
 const url = 'https://bit.ly';
-const to = location.pathname;
+const to = location.pathname.slice(11);
 const params = location.search;
 const hash = location.hash;
 const redirect = url + to + params + hash;
